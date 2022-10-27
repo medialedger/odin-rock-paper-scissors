666fe9401c54d5081ddc10d57066bd056d152039
Richard Ledger <richard.ledger@lanebryant.com>
2022-10-27 12:26:36 -0400

    Add UI to the core game functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const resultsBox = document.querySelector('.results');
+const choiceButtons = document.querySelectorAll('.btn-box button');
+choiceButtons.forEach(btn => {
+	btn.addEventListener('click',playRound);
+})
+const newButton = document.querySelector('.new-game');
+const scores = document.querySelectorAll('.score');
+const roundNum = document.querySelector('.round-num');
+
 function getComputerChoice() {
 	const random =  Math.floor(Math.random() * 3) + 1;
 	switch (random) {
@@ -15,44 +24,69 @@ function getComputerChoice() {
 	}
 }
 
-function playRound(playerSelection, computerSelection) {
+function getRoundResults(playerSelection, computerSelection) {
 	if(playerSelection === computerSelection) {
+		sessionStorage.setItem('tie', parseInt(sessionStorage.getItem('tie')) + 1);
 		return 'Tied! Try again.';
 	} else if(playerSelection === 'rock') {
 		if(computerSelection === 'paper') {
+			sessionStorage.setItem('lose', parseInt(sessionStorage.getItem('lose')) + 1);
 			return 'You lost! Paper covers rock.';
 		} else if(computerSelection === 'scissors') {
+			sessionStorage.setItem('win', parseInt(sessionStorage.getItem('win')) + 1);
 			return 'You won! Rock smashes scissors.';
 		}
 	} else if(playerSelection === 'paper') {
 		if(computerSelection === 'rock') {
+			sessionStorage.setItem('win', parseInt(sessionStorage.getItem('win')) + 1);
 			return 'You won! Paper covers rock.';
 		} else if(computerSelection === 'scissors') {
+			sessionStorage.setItem('lose', parseInt(sessionStorage.getItem('lose')) + 1);
 			return 'You lost! Scissors cut paper.';
 		}
 	} else if (playerSelection === 'scissors') {
 		if(computerSelection === 'rock') {
+			sessionStorage.setItem('lose', parseInt(sessionStorage.getItem('lose')) + 1);
 			return 'You lost! Rock smashes scissors.';
 		} else if(computerSelection === 'paper') {
+			sessionStorage.setItem('win', parseInt(sessionStorage.getItem('win')) + 1);
 			return 'You won! Scissors cut paper.';
 		}
 	}
 }
 
-function playGame() {
-	for (let i = 0; i < 5; i++) {
-		let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
-		checkPlayerSelection(playerSelection);
+function updateScore() {
+	scores.forEach(score => {
+		score.textContent = sessionStorage.getItem(score.dataset.score);
+	})
+}
+
+function newRound() {
+	sessionStorage.setItem('round',parseInt(sessionStorage.getItem('round')) + 1);
+	roundNum.textContent = sessionStorage.getItem('round');
+}
+
+function playRound() {
+	let results = getRoundResults(this.dataset.choice, getComputerChoice());
+	resultsBox.insertAdjacentHTML('beforeend',`<li><span class="round">Round ${sessionStorage.getItem('round')}:</span> ${results}</li>`);
+	updateScore();
+	newRound();
+	if(newButton.hidden === true){
+		newButton.hidden = false;
 	}
 }
 
-function checkPlayerSelection(selection) {
-	if (selection === 'rock' || selection === 'paper' || selection === 'scissors') {
-		console.log(playRound(selection, getComputerChoice()));
-	} else {
-		selection = prompt('Try again! Must be either rock, paper or scissors.').toLowerCase();
-		checkPlayerSelection(selection);
-	}
+function newGame() {
+	sessionStorage.setItem('win', 0);
+	sessionStorage.setItem('lose', 0);
+	sessionStorage.setItem('tie', 0);
+	sessionStorage.setItem('round', 1);
+	const resultsBox = document.querySelector('.results');
+	resultsBox.innerHTML = '';
+	newButton.hidden = true;
+	updateScore();
+	roundNum.textContent = sessionStorage.getItem('round');
 }
+newButton.addEventListener('click',newGame);
 
-playGame();
+newGame();
